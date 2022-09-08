8a3242c8b7df73e5bd42691c27d959f3ed344310
Gifti Mulugeta <gakmt7@gmail.com>
2022-09-08 16:08:20 -0700

    C Pogramming

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,7 +7,6 @@
  */
 int main(void)
 {
-	fprintf(stderr, "and that piece of art is useful
-			\" - Dora Korpar, 2015-10-19");
+	write(2, "and that piece of art is useful - Dora Korpar, 2015-10-19\n", 58);
 	return (1);
 }
