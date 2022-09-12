44e93c9f2fc1ce49e5b1c0addc3ffc0e704e50d9
Gifti Mulugeta <gakmt7@gmail.com>
2022-09-12 04:19:51 -0700

    variables

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+/**
+ *main - Entry point
+ *
+ * Return: Always 0(Success)
+ */
+int main(void)
+{
+	int c;
+	int d;
+	int e;
+
+	for (c = '0'; c < '9' ; c++)
+	{
+		for (d = c + 1 ; d < '9'; d++)
+		{
+			for (e = d + 1 ; e <= '9';  e++)
+			{
+				if (c != d)
+				{
+					putchar(c);
+					putchar(d);
+					putchar(e);
+					if (c == '8' && d == '9')
+						continue;
+					putchar(',');
+					putchar(' ');
+				}
+               		}
+	
+		}
+	}
+	putchar('\n');
+	return (0);
+}
+
+
+
