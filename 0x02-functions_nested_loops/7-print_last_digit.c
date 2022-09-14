320bec70edb599b7ff8ed1892b38872bd61bbfd8
Gifti Mulugeta <gakmt7@gmail.com>
2022-09-14 12:43:26 -0700

    Functions

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+
+/**
+ * print_last_digit - Entry point
+ * @n: The character to be print
+ *
+ *Return: Always 0
+ */
+int print_last_digit(int n)
+{
+	int x;
+
+	x = n % 10;
+
+	if (n >= 0)
+	{
+		x = n % 10;
+	}
+	else
+	{
+		x = (-1) * n % 10;
+	}
+	_putchar(x + '0');
+	return (x);
+}
