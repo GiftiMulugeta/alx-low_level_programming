2a1fb7afc96d71bdee0100e83abdaae49047a38d
Gifti Mulugeta <gakmt7@gmail.com>
2022-09-14 15:03:46 -0700

    Functions

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ *
+ *
+ *
+ *
+ */
+int main(void)
+{
+	int n;
+	int sum=0;
+
+	for (n = 0; n<1024; n++)
+	{
+		if ((n%3==0)||(n%5==0))
+		{
+			sum=sum+n;
+			printf("%d",sum);
+		}
+		
+	}
+	printf("\n");
+	return (0);
+}
