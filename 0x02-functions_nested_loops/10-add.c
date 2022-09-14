4bfd2f8a9b4cf8c593ecd5036e4bdf25cbcded19
Gifti Mulugeta <gakmt7@gmail.com>
2022-09-14 13:24:11 -0700

    Functions

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+
+/**
+ * add - Entry point
+ * @x: The character to be print
+ *@y: The character to be print
+ *
+ *Return: Always 0
+ */
+int add(int x, int y)
+{
+	return (x + y);
+}
