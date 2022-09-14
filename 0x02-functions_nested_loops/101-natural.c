f2970363d5139b69cbfec85420604bedd38c3a0e
Gifti Mulugeta <gakmt7@gmail.com>
2022-09-14 15:18:13 -0700

    Functions

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,26 +1,23 @@
 #include <stdio.h>
 
 /**
+ * main - Entry point
  *
- *
- *
- *
+ * Return: Always 0
  */
 int main(void)
 {
 	int n;
-	int sum=0;
+	int sum = 0;
 
-	for (n = 0; n<1024; n++)
+	for (n = 0; n < 1024; n++)
 	{
-		if ((n%3==0)||(n%5==0))
+		if ((n % 3 == 0) || (n % 5 == 0))
 		{
-			sum=sum+n;
-			
+			sum = sum + n;
 		}
-		
 	}
-	printf("%d",sum);
+	printf("%d", sum);
 	printf("\n");
 	return (0);
 }
