4ff06b6ee9a6d380e88d472015676c5af64e45ba
Gifti Mulugeta <gakmt7@gmail.com>
2022-09-14 14:50:17 -0700

    Functions

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+
+
+/**
+ * print_to_98 - Entry point
+ * @n: The character to be print
+ *
+ *Return: Always 0
+ */
+void print_times_table(int n)
+{
+	int x, y;
+
+	if (n >= 0 && n <= 15)
+	{
+		for (x = 0; x <= n; x++)
+		{
+			_putchar('0');
+
+			for ( y = 1; y <= x; y++)
+			{
+
+		}
+}
