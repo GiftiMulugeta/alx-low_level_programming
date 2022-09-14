bcddbfcc6bd2735c3f5329653428d725869a13c8
Gifti Mulugeta <gakmt7@gmail.com>
2022-09-14 14:13:15 -0700

    Functions

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <stdio.h>
+
+
+/**
+ * print_to_98 - Entry point
+ * @n: The character to be print
+ *
+ *Return: Always 0
+ */
+void print_to_98(int n)
+{
+	int x;
+
+	if (n < 98)
+		for (x = n; x <= 98; x++)
+		{
+			printf("%d", x);
+			if (x != 98)
+				printf(",");
+			printf(" ");
+		}
+	else
+		for (x = n; x >= 98; x--)
+		{
+			printf("%d", x);
+			if (x != 98)
+				printf(",");
+			printf(" ");
+		}
+	printf("\n");
+}
