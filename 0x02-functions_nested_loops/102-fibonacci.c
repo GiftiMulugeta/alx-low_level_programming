0cd6ef081bca05dc15826176be1e9c988441d0b9
Gifti Mulugeta <gakmt7@gmail.com>
2022-09-14 16:33:18 -0700

    Functions

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -7,20 +7,19 @@
  */
 int main(void)
 {
-	int n1=1, n2=2, n, num;
-	char c=',';
-	printf("%d%c%c%d%c%c",n1,c,' ',n2,c,' ');
-	for (n=3; n<50; n++)
+	long int n1 = 1, n2 = 2, n, num;
+
+	char c = ',';
+
+	printf("%ld%c%c%ld%c%c", n1, c, ' ', n2, c, ' ');
+	for (n = 3; n <= 50; n++)
 	{
-		num= n1+ n2;
-	        printf("%d",num); 
-		if (num != -811192543)
-		{
-			printf("%c",c); 
-			printf(" ");
-		}
-		n1= n2;
-		n2=num;
+		num = n1 + n2;
+		printf("%ld", num);
+		printf("%c", c);
+		printf(" ");
+		n1 = n2;
+		n2 = num;
 	}
 	printf("\n");
 
