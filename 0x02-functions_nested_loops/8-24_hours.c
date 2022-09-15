2b1a29d189c077da32fc47e6b9db7e42bd56f0f0
Gifti Mulugeta <gakmt7@gmail.com>
2022-09-14 17:46:50 -0700

    Function

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * jack_bauer -  Entry point
+ *
+ *
+ */
+void jack_bauer(void)
+{
+	int hr, mi;
+
+	for (hr=0; hr <= 23; hr++)
+	{
+		for ( mi = 0; mi <= 59; mi++)
+		{
+			_putchar((hr/10) + '0');
+			_putchar((hr%10) + '0'); 
+			_putchar(':'); 
+			_putchar((mi/10) + '0'); 
+			_putchar((mi%10) + '0'); 
+			_putchar('\n');
+		}
+	}
+}
