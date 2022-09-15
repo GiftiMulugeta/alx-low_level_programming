9cbf0414883442d1fe532603d37b6cedd4547206
Gifti Mulugeta <gakmt7@gmail.com>
2022-09-14 17:50:34 -0700

    Functions

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -9,15 +9,15 @@ void jack_bauer(void)
 {
 	int hr, mi;
 
-	for (hr=0; hr <= 23; hr++)
+	for (hr = 0; hr <= 23; hr++)
 	{
-		for ( mi = 0; mi <= 59; mi++)
+		for (mi = 0; mi <= 59; mi++)
 		{
-			_putchar((hr/10) + '0');
-			_putchar((hr%10) + '0'); 
-			_putchar(':'); 
-			_putchar((mi/10) + '0'); 
-			_putchar((mi%10) + '0'); 
+			_putchar((hr / 10) + '0');
+			_putchar((hr % 10) + '0');
+			_putchar(':');
+			_putchar((mi / 10) + '0');
+			_putchar((mi % 10) + '0');
 			_putchar('\n');
 		}
 	}
