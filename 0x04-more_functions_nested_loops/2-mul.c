7c1de3c83fdbf2049ea4842b38478cf7b0eb5356
Gifti Mulugeta <gakmt7@gmail.com>
2022-09-15 06:57:44 -0700

    More about function

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * mul- Entry point
+ *@a: The character to print
+ *@b: The character to print
+ *
+ * Return: Always 0
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
