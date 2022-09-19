36b0a241865144c4de933208c3ca0bc4932fdbc9
Gifti Mulugeta <gakmt7@gmail.com>
2022-09-19 09:28:03 -0700

    Pointers and arrays

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,40 @@
+#include "main.h"
+#include "stdio.h"
+#include <string.h>
+
+/**
+ *puts_half - Entry point
+ *@str: the character to be print
+ *
+ * Return: Always 0.
+ */
+void puts_half(char *str)
+{
+	int c, d, len;
+
+	d = 0;
+
+	len = strlen(str);
+	while (str[d] != '\0')
+	{
+		d++;
+	}
+
+	if (len % 2 == 0)
+	{
+		c = len / 2;
+		for (; c <= d; c++)
+		{
+			_putchar(str[c]);
+		}
+	}
+	else if (len % 2 != 0)
+	{
+		c = (len - 1) / 2;
+		for (; c <= d; c++)
+		{
+			_putchar(str[c]);
+		}
+	}
+	_putchar('\n');
+}
