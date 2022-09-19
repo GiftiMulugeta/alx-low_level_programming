6afa7ac99f9645596649f4c798ab82b52db7284c
Gifti Mulugeta <gakmt7@gmail.com>
2022-09-19 07:34:11 -0700

    Pointers and arrays

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include "stdio.h"
+
+/**
+ *puts2 - Entry point
+ *@str: the character to be print
+ *
+ * Return: Always 0.
+ */
+void puts2(char *str)
+{
+	int c;
+
+	c = 0;
+	while (str[c] != '\0')
+	{
+		_putchar(str[c]);
+		c = c + 2;
+
+	}
+	_putchar('\n');
+
+}
