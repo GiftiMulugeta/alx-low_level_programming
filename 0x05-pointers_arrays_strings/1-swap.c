f0161e746e52042918a2f5755ebb4fc5892a4e33
Gifti Mulugeta <gakmt7@gmail.com>
2022-09-19 03:08:41 -0700

    Pointer and arrays

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include "stdio.h"
+
+/**
+ *swap_int - check the code
+ *@a: the character to be print
+ *@b: the character to print
+ *
+ * Return: Always 0.
+ */
+void swap_int(int *a, int *b)
+{
+	int *p;
+	int *q;
+
+	p = a;
+	*p = 42;
+	q = b;
+	*q = 98;
+}
