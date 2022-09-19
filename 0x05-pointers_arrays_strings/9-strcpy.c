876f912965c4e220eba6d1cb92c41eb7b4fcde32
Gifti Mulugeta <gakmt7@gmail.com>
2022-09-19 12:50:42 -0700

    Pointera and arrays

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -11,10 +11,10 @@
  */
 char *_strcpy(char *dest, char *src)
 {
-	int i=0;
+	int i = 0;
 
-	for(; src[i]!='\0';i++)
-		dest[i]=src[i];
-	dest[i]='\0';
+	for (; src[i] != '\0'; i++)
+		dest[i] = src[i];
+	dest[i] = '\0';
 	return (dest);
 }
