6de4ccb83bb1ea1d2e6941b82bc72aeecbb2ba81
Gifti Mulugeta <gakmt7@gmail.com>
2022-09-19 03:00:23 -0700

    Pointerand arrays

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include "stdio.h"
+
+/**
+ * reset_to_98 - check the code
+ *@n: the character to be print
+ *
+ * Return: Always 0.
+ */
+void reset_to_98(int *n)
+{
+	int *p;
+
+	p = n;
+	*p = 98;
+}
