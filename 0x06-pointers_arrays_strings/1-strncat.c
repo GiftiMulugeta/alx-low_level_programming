43f0d7e30189a1a6c802859535bda931d9476c27
Gifti Mulugeta <gakmt7@gmail.com>
2022-09-22 07:10:11 -0700

    More about pointer

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,38 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+/**
+ * *_strcat- check the code
+ * @dest: the character to print
+ * @src: the character to print
+ *
+ * Return: Always 0
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	char *con = dest;
+	int i = 0;
+	int j = 0;
+	int srclength;
+	int s;
+
+	srclength = strlen(src);
+	if (n>srclength)
+		n=srclength;
+	else
+		n=n;
+	while (dest[i] != '\0')
+	{
+		con[j] = dest[i];
+		i++;
+		j++;
+	}
+	i = 0;
+	for (s=0; s<n; s++)
+	{
+		con[j] = src[s];
+		j++;
+	}
+	con[j] = '\0';
+	return (con);
+}
