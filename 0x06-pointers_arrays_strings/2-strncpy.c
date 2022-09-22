7b2bb6f8fd86ced7c95a9029db8d0971e1c3c658
Gifti Mulugeta <gakmt7@gmail.com>
2022-09-22 07:31:19 -0700

    More about pointers

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,47 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+/**
+ * *_strncat- check the code
+ * @dest: the character to print
+ * @src: the character to print
+ *@n: the character to print
+ *
+ * Return: Always 0
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	/*char *con = dest;*/
+	int i = 0;
+	
+	int srclength;
+	int s;
+
+	srclength = strlen(src);
+	if (n > srclength)
+		n = srclength;
+	else
+		n = n;
+	/*
+	while (dest[i] != '\0')
+	{
+		con[j] = dest[i];
+		i++;
+		j++;
+	}
+	i = 0;
+	for (s = 0; s < n; s++)
+	{
+		con[j] = src[s];
+		j++;
+	}
+	con[j] = '\0';*/
+
+	for(s=0; s<n; s++)
+	{
+		dest[i]=src[s];
+		i++;
+	}
+	
+	return (dest);
+}
