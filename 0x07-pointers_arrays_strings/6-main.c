8854d6b414a1c698e4bca623585c8b9c6bf1ea9d
Gifti Mulugeta <gakmt7@gmail.com>
2022-09-26 08:44:54 -0700

    a function that prints the chessboard.

---
+++ 0x07-pointers_arrays_strings/6-main.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _memcpy - check the code
+ * @dest : prints buffer in hexa
+ * @src: the address of memory to print
+ * @n: the size of the memory to print
+ *
+ * Return: Nothng
+ */
+int main(void)
+{
+	char board[8][8] = {
+		{'r', 'k', 'b', 'q', 'k', 'b', 'k', 'r'},
+		{'p', 'p', 'p', 'p', 'p', 'p', 'p', 'p'},
+		{' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+		{' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+		{' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+		{' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+		{'P', 'P', 'P', 'P', 'P', 'P', 'P', 'P'},
+		{'R', 'K', 'B', 'Q', 'K', 'B', 'K', 'R'},
+	};
+	print_chessboard(board);
+	return (0);
+}
