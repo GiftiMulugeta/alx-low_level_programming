d65b67ca1c51646fbfd95dfd8ec19d3090cb190a
Gifti Mulugeta <gakmt7@gmail.com>
2022-09-28 11:59:33 -0700

    About recurssion

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -1,37 +1,15 @@
 #include "main.h"
 #include <stdio.h>
 #include <string.h>
-int palidrome(char *str, int i, int j);
 /**
  * is_palindrome - return factorial of a number
  * @s: the character to print
  *
  * Return: Always 0
  */
-int is_palindrome(char *s)
+int wildcmp(char *s1, char *s2)
 {
-	if (strlen(s) <= 1)
-		return (1);
-	return (palidrome(s, 0, strlen(s) - 1));
-
-}
-
-/**
- * palidrome  - return square root
- * @str: the character to print
- * @i: the character to print
- * @j: the character to print
- *
- * Return: Always 0.
- */
-int palidrome(char *str, int i, int j)
-{
-	i =  0;
-	j = strlen(str) - 1;
-	if (str[i] != str[j])
+	if (s1 != s2)
 		return (0);
-	else if (i >= j)
-		return (1);
-	return (palidrome(str, i + 1, j - 1));
-
+	return (wildcmp(s1 + 1, s2 + 1));
 }
