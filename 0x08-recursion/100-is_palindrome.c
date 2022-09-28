748df72d45899e9e4d9f7f64cb9ac6fed1295aa6
Gifti Mulugeta <gakmt7@gmail.com>
2022-09-28 10:58:17 -0700

    About recurssion

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+int prime_num(int num, int prime);
+/**
+ * is_prime_number - return factorial of a number
+ * @n: the character to print
+ *
+ * Return: Always 0
+ */
+int is_palindrome(char *s)
+{
+	if (n <= 1)
+		return (0);
+	return (prime_num(n, 2));
+
+}
+
+/**
+ * prime_num  - return square root
+ * @num: the character to print
+ * @prime: the character to print
+ *
+ * Return: Always 0.
+ */
+int prime_num(char *str, int i, int j)
+{
+	i =  0; j=
+}
