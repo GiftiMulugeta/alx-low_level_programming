d0f0826160b3ab294fb7e6b00fe4042eb45dd324
Gifti Mulugeta <gakmt7@gmail.com>
2022-09-30 05:44:51 -0700

    argc and argv

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - check the code
+ * @argc: the character to print
+ * @argv: the character to print
+ * Return: Always 0.
+ */
+int main(int argc, char *argv[])
+{
+	int mul, i;
+
+	if (argc == 3)
+	{
+		for (i = 1; i < argc; i++)
+		{
+			mul = atoi(argv[1]) * atoi(argv[2]);
+		}
+		printf("%d", mul);
+	}
+	else
+	{
+		printf("error\n");
+		return (1);
+	}
+
+	putchar('\n');
+	return (0);
+}
