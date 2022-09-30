c58ab0588ada0608f4800d4cd4cc38e1b1f9fa69
Gifti Mulugeta <gakmt7@gmail.com>
2022-09-30 05:04:34 -0700

    argc and argv

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,16 +1,20 @@
 #include <stdio.h>
 
-/**main - check the code
- *
+/**
+ * main - check the code
+ * @argc: the character to print
+ * @argv: the character to print
  * Return: Always 0.
  */
 int main(int argc, char *argv[])
 {
 	int i;
-	if (argc > 1)
+
+	if (argc >= 1)
 	{
-		for (i=1;i<argc;i++)
-			printf("%s",argv[i]);
+		for (i = 0; i < argc; i++)
+			printf("%s", argv[i]);
 	}
+	putchar('\n');
 	return (0);
 }
