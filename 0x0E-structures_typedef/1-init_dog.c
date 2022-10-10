fb3d29339dd2c2ba9ea7ff39cb06fd9a94a55be7
Gifti Mulugeta <gakmt7@gmail.com>
2022-10-10 10:42:20 -0700

    Struct

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "dog.h"
+
+/**
+ * init_dog - initialize struct
+ * @d: member1
+ * @name: member2
+ * @age: eember3
+ * @owner: member4
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
