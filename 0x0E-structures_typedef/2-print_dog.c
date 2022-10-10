b42c53016d9c6aa095dc4a7a2dba85244304beca
Gifti Mulugeta <gakmt7@gmail.com>
2022-10-10 11:07:54 -0700

    Struct

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "dog.h"
+
+/**
+ * print_dog - prints struct dog
+ * @d: member1
+ */
+void print_dog(struct dog *d)
+{
+	if (d == NULL)
+		printf("nil");
+	if (d->name == NULL)
+		printf("nil");
+	else
+		printf("Name: %s\n", d->name);
+	printf("Age: %.1f\n", d->age);
+	printf("Owner: %s\n", d->owner);
+}
