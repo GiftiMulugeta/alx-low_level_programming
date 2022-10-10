e976b7f0280581cf1347341f0c6fa64b2ab75eb9
Gifti Mulugeta <gakmt7@gmail.com>
2022-10-10 12:57:00 -0700

    Struct

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "dog.h"
+#include <stdlib.h>
+void free_dog(dog_t *d)
+{
+	if (d==NULL)
+		return;
+	if (d->name!=NULL)
+		free(d->name);
+	if (d->owner!= NULL)
+		free(d->owner);
+	free(d);
+	
+}
