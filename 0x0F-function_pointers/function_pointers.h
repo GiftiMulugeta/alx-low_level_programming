e55d8bd36ec1b2a4de4e84e1df219eb4bb5f4a29
Gifti Mulugeta <gakmt7@gmail.com>
2022-10-12 09:17:42 -0700

    Function pointer

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,3 +1,7 @@
+#ifndef FUNCTION_POINTERS_H
+#define FUNCTION_POINTERS_H
+
+
 int _putchar(char c);
 
 
@@ -9,5 +13,5 @@ void array_iterator(int *array, size_t size, void (*action)(int));
 
 int int_index(int *array, int size, int (*cmp)(int));
 
-
+#endif
 
