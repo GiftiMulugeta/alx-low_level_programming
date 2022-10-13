d667956f51324158106b926419b3b88d24f38691
Gifti Mulugeta <gakmt7@gmail.com>
2022-10-13 13:32:47 -0700

    Vardiac function

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -38,12 +38,11 @@ void print_all(const char * const format, ...)
 					printf("%s", string);
 					break;
 				}
-				else
-					printf("(nil)");
+				printf("(nil)");
 				break;
 		}
-		if ((format[i] == 'c' || format[i] == 'i' ||
-		format[i] == 'f' || format[i] == 's') && format[(i + 1)] != '\0')
+		if ((format[i] == 'c' || format[i] == 'i' || format[i] == 'f'
+			|| format[i] == 's') && format[(i + 1)] != '\0')
 			printf(", ");
 		i++;
 	}
