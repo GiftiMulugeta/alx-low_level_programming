309cca005090c503d011221f8788ce667193adef
Gifti Mulugeta <gakmt7@gmail.com>
2022-10-13 11:39:35 -0700

    Variadic function

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -18,7 +18,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	{
 		string = va_arg(a, char*);
 		if (string ==NULL)
-			printf("nil");
+			printf("(nil)");
 		else
 			printf("%s", string);
 		if (separator == NULL)
