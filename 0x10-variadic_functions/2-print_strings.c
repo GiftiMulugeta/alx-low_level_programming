a1bd91542b064e8bdcdff44bc2ebca73fe05eac1
Gifti Mulugeta <gakmt7@gmail.com>
2022-10-13 11:34:58 -0700

    Vardiac function

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include "variadic_functions.h"
+#include <stdarg.h>
+/**
+ * print_strings - return sum of arguments
+ * @separator: first parameter
+ * @n: an integer
+ * @..: other parametrs
+ */
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	va_list a;
+	unsigned int i;
+	char *string;
+
+	va_start(a, n);
+	for (i = 0; i < n; i++)
+	{
+		string = va_arg(a, char*);
+		printf("%s", string);
+		if (string == NULL)
+			printf("nill");
+		if (separator == NULL)
+			continue;
+		if (i < n - 1)
+			printf("%s", separator);
+	}
+	printf("\n");
+	va_end(a);
+}
