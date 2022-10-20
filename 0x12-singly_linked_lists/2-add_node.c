795f5025a88dad46fdbdfabd1a5e4a0a05685857
Gifti Mulugeta <gakmt7@gmail.com>
2022-10-20 06:02:39 -0700

    add ana element at the beginnig of the list

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "lists.h"
+/**
+ * add_node - add an element at the beginning of the list
+ * @head: argument
+ * @str: argument
+ *
+ * Return: Returs number of nodes.
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *new;
+	unsigned int leng = 0;
+
+	if (new == NULL)
+		return (NULL);
+	while (str[leng] != '\0')
+		leng++;
+	new = malloc(sizeof(list_t));
+	new->str = strdup(str);
+	new->next = *head;
+	*head = new;
+	new->len = leng;
+	return (new);
+}
