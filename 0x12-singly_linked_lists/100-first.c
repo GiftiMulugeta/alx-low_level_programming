4d2be41ac7b727552ba471610c29e28bc3b22f5a
Gifti Mulugeta <gakmt7@gmail.com>
2022-10-20 07:21:22 -0700

    printd a statement

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "lists.h"
+/**
+ * prints - print an argument
+ *
+ * Return: Always 0.
+ */
+int prints(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+	return (0);
+}
