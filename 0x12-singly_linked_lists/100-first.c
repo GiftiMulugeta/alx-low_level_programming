f663526bf38bde29f0589a7ca3523a113b69cae9
Gifti Mulugeta <gakmt7@gmail.com>
2022-10-20 07:53:32 -0700

    prints a statement

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include "lists.h"
+
+int prints(void) __attribute__((constructor));
 /**
  * prints - print an argument
  *
@@ -9,5 +11,5 @@ int prints(void)
 {
 	printf("You're beat! and yet, you must allow,\n");
 	printf("I bore my house upon my back!\n");
-	return (0);
+	return(0);
 }
