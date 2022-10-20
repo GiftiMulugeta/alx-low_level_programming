379a3eca2d8414c11103964e278e14d9e0eb2180
Gifti Mulugeta <gakmt7@gmail.com>
2022-10-20 08:59:40 -0700

    free head

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+void free_list(list_t *head)
+{
+	list_t *tmp;
+
+	tmp = head;
+
+	while (head)
+	{
+		tmp = head;
+		head = head->next;
+		free(tmp->str);
+		free(tmp);
+	}
+	free(head);
+}
