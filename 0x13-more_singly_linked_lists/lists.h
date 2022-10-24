3b250ec62b0f144f2af5df55ae576ad9b93ce1a6
Gifti Mulugeta <gakmt7@gmail.com>
2022-10-24 11:53:56 -0700

    More singly linked list

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -1,6 +1,12 @@
 #ifndef _LISTS_H
 #define _LISTS_H
 
+/**
+ * struct listint_s - singly linked list
+ * @n: integer
+ * @next: points to the next node
+ * Description: singly linked list node structure
+ */
 typedef struct listint_s
 {
 	int n;
