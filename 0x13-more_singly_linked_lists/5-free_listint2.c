0eb05fc3ca123e1ccd2d68be31663622152d0794
Gifti Mulugeta <gakmt7@gmail.com>
2022-10-24 15:32:08 -0700

    More singly linked list

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * free_listint2 - free listiny linked list
+ * @head: argument
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *tmp;
+
+	if (*head == NULL)
+		printf("(nil)");
+	while (*head)
+	{
+		tmp = *head;
+		*head = (*head)->next;
+		free(tmp);
+	}
+	*head = NULL;
+
+}
