35dde0ad754aeb2d67abb0947ff3e596659af79b
Gifti Mulugeta <gakmt7@gmail.com>
2022-10-24 13:01:28 -0700

    More singly linked list

---
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -0,0 +1,34 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include "lists.h"
+/**
+ * add_nodeint_end - prints elements of listin_n
+ * @head: argument
+ * @n: argument
+ * Return: number of nide
+ */
+listint_t *add_nodeint_end(listint_t **head, const int n)
+{
+	listint_t *new;
+	listint_t *tmp;
+
+	new = malloc(sizeof(listint_t));
+	new->n = n;
+	if (new == NULL)
+		return (NULL);
+	if (*head == NULL)
+	{
+		new->next = *head;
+		*head = new;
+	}
+	else
+	{
+		new->next = NULL;
+		tmp = *head;
+		while ((tmp->next) != NULL)
+			tmp = tmp->next;
+		tmp->next = new;
+	}
+	return (new);
+}
