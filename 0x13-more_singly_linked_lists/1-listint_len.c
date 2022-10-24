f85058f05b26ef04f50a3bbfb0230495089aa129
Gifti Mulugeta <gakmt7@gmail.com>
2022-10-24 12:07:20 -0700

    More singly linked list

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include "lists.h"
+/**
+ * listint_len - prints elements of listin_n
+ * @h: argument
+ * Return: number of nide
+ */
+size_t listint_len(const listint_t *h)
+{
+	unsigned int node = 0;
+
+	while (h)
+	{
+		h = h->next;
+		node++;
+	}
+	printf("\n");
+		return (node);
+}
