f7667b97e2ace2ebe75527e2f30e2c8011347720
Gifti Mulugeta <gakmt7@gmail.com>
2022-10-24 13:04:13 -0700

    More singly linked list

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -13,10 +13,10 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 	listint_t *new;
 	listint_t *tmp;
 
-	new = malloc(sizeof(listint_t));
-	new->n = n;
+	new =malloc(sizeof(listint_t));
 	if (new == NULL)
 		return (NULL);
+	new->n = n;
 	if (*head == NULL)
 	{
 		new->next = *head;
