47d4abe9112a96999c81f5f5b68e416b4158f801
Gifti Mulugeta <gakmt7@gmail.com>
2022-10-24 18:47:05 -0700

    More singly linked list

---
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include "lists.h"
+/**
+ * sum_listint - return element at the given index
+ * @head: argument
+ * Return: NULL
+ */
+int sum_listint(listint_t *head)
+{
+	int i;
+	int sum = 0;
+	listint_t *tmp;
+
+	if (head == NULL)
+		return (0);
+	tmp = head;
+	while (tmp->next != NULL)
+	{
+		tmp = tmp->next;
+		i = tmp->n;
+		sum += i;
+	}
+	return (sum);
+}
